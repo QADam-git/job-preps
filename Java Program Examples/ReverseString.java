d5253d5e5dec1891f34fb53d5408580a859bbafc
QADam-git <anadamini96@gmail.com>
2026-02-13 12:13:06 +0530

    Create ReverseString.java

---
+++ Java Program Examples/ReverseString.java
@@ -0,0 +1,24 @@
+
+public class ReverseString {
+	public static void main(String[] args) {
+		String str = "Apple";
+		System.out.println(FnReverse(str));
+	}
+
+	// Function to reverse the input string
+	public static String FnReverse(String inputString) {
+		if (inputString == null) {
+			throw new IllegalArgumentException("Null is not valid input");
+		}
+
+		// StringBuilder outputString = new StringBuilder();
+		String outputString = "";
+		char[] chars = inputString.toCharArray();
+
+		for (int i = chars.length - 1; i >= 0; i--) {
+			// outputString.append(chars[i]);
+			outputString = outputString + chars[i];
+		}
+		return outputString.toString();
+	}
+}
